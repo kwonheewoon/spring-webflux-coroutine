70df35677d708e20805fa62896f78fce20d51e50
dybala <dybala.arg@dktechin.com>
2024-01-18 18:55:40 +0900

    grpc proto파일 및 컴파일에 필요한 gradle 의존성 정의

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val mapstruct_version: String by project
-val r2dbc_postgresql: String by project
+val mapstructVersion: String by project
+val r2dbcPostgresql: String by project
+val grpcVersion: String by project
+val grpcKotlinVersion: String by project
+val grpcProtoVersion: String by project
 
 plugins {
     id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.google.protobuf") version "0.9.4"
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.spring") version "1.9.21"
     kotlin("kapt") version "1.9.21"
@@ -24,7 +29,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("io.r2dbc:r2dbc-postgresql:$r2dbc_postgresql")
+    implementation("io.r2dbc:r2dbc-postgresql:$r2dbcPostgresql")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
@@ -41,9 +46,15 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     // mapstruct
-    implementation("org.mapstruct:mapstruct:$mapstruct_version")
-    kapt("org.mapstruct:mapstruct-processor:$mapstruct_version")
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
     //testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    // protobuf
+    runtimeOnly("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion") // protobuf-java로 만들어지는 클라이언트 입장의 파일에서 필요한 메서드 등을 포함하고 있는 의존성
+    implementation("io.grpc:grpc-protobuf:$grpcProtoVersion") // protobuf-java로 만들어지는 서버 입장의 파일에서 필요한 메서드 등을 포함하고 있는 의존성
+    implementation("com.google.protobuf:protobuf-kotlin:$grpcVersion") // protocol buffer를 코틀린 파일로 컴파일하는데 사용되는 의존성
+    implementation("io.grpc:grpc-netty:$grpcProtoVersion")
 
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
@@ -51,6 +62,43 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+}
+
+protobuf {
+    protoc { // protocol buffer를 컴파일하는 protoc의 라이브러리를 지정
+        artifact = "com.google.protobuf:protoc:$grpcVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcProtoVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
+sourceSets{
+    getByName("main"){
+        java {
+            srcDirs(
+                "build/generated/source/proto/main/grpckt",
+                "build/generated/source/proto/main/java",
+                "build/generated/source/proto/main/grpc"
+            )
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
